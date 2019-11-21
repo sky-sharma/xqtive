296f3149baf54210207f3b8d105c81c2071fbc30
skysharma <sky.sharma@gmail.com>
2019-11-21 10:30:00 -0600

    1. In XqtiveExampleStateMachine class' __init__, set self.cust_hi_priorities BEFORE calling States.__init__. This way the merging of priority dicts can be done in the parent class. 2. Renamed "Message" state to "MESSAGE" (upper case). 3. Added "PWR_SPLY_OFF", "PWR_SPLY_OUTPUT", "SetCurrentLimit", "SetVoltage", "CloseContactor" states.

--- xqtive_example/modules/xqtive_example_state_machine.py
+++ xqtive_example/modules/xqtive_example_state_machine.py
@@ -2,9 +2,29 @@ from xqtive_states import States
 
 class XqtiveExampleStateMachine(States):
     def __init__(self, config):
+        self.cust_hi_priorities = {"PWR_SPLY_OFF": 0}
         States.__init__(self, config)
-        new_state_priorities = {"PowerOff": 0}
-        self.hi_priorities.update(new_state_priorities)
 
-    def Message(self):
+    def MESSAGE(self):
         print(f"Message {self.__dict__}")
+
+    def PWR_SPLY_OUTPUT(self, params):
+        voltage_to_set = float(params[0])
+        next_states_params = [
+        ["SetCurrentLimit", 1],
+        ["SetVoltage", voltage_to_set],
+        ["WAIT", "20"],
+        ["CloseContactor"]]
+        return next_states_params
+
+    def PWR_SPLY_OFF(self):
+        print("Turning off power!")
+
+    def SetCurrentLimit(self, params):
+        print(f"Setting current limit to {params[0]} Amps.")
+
+    def SetVoltage(self, params):
+        print(f"Setting voltage out to {params[0]} Volts.")
+
+    def CloseContactor(self):
+        print(f"Closing contactor.")
