bc139d79cec91e1664864deaec039188731007ac
skysharma <sky.sharma@gmail.com>
2019-11-21 15:56:25 -0600

    1. Added more states to self.cust_hi_priorities and set priority values to "High" and "Highest" rather than 0 and 1. 2. Added a print to "PWR_SPLY_OUTPUT". 3. Added next_states_params to return from "PWR_SPLY_OFF". 4. Added "E_STOP". 5. Added "OpenContactor".

--- xqtive_example/modules/xqtive_example_state_machine.py
+++ xqtive_example/modules/xqtive_example_state_machine.py
@@ -2,7 +2,7 @@ from xqtive import XqtiveStates
 
 class XqtiveExampleStateMachine(XqtiveStates):
     def __init__(self, config):
-        self.cust_hi_priorities = {"PWR_SPLY_OFF": 0}
+        self.cust_hi_priorities = {"PWR_SPLY_OUTPUT": "Normal", "PWR_SPLY_OFF": "High", "E_STOP": "Highest"}
         XqtiveStates.__init__(self, config)
 
     def MESSAGE(self):
@@ -10,6 +10,7 @@ class XqtiveExampleStateMachine(XqtiveStates):
 
     def PWR_SPLY_OUTPUT(self, params):
         voltage_to_set = float(params[0])
+        print(f"Setting output voltage to {voltage_to_set}")
         next_states_params = [
         ["SetCurrentLimit", 1],
         ["SetVoltage", voltage_to_set],
@@ -19,6 +20,16 @@ class XqtiveExampleStateMachine(XqtiveStates):
 
     def PWR_SPLY_OFF(self):
         print("Turning off power!")
+        next_states_params = [
+        ["SetVoltage", 0],
+        ["WAIT", "5"],
+        ["OpenContactor"]]
+        return next_states_params
+
+    def E_STOP(self):
+        print("Hitting E-Stop!")
+        next_state_params = ["OpenContactor"]
+        return next_state_params
 
     def SetCurrentLimit(self, params):
         print(f"Setting current limit to {params[0]} Amps.")
@@ -28,3 +39,6 @@ class XqtiveExampleStateMachine(XqtiveStates):
 
     def CloseContactor(self):
         print(f"Closing contactor.")
+
+    def OpenContactor(self):
+        print(f"Opening contactor.")
