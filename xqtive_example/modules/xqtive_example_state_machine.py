1c000d57b510d7afcc508cb8b715f44234a8729d
skysharma <sky.sharma@gmail.com>
2019-11-21 06:09:22 -0600

    1. Added "config" to XqtiveExampleStateMachine class' __init__. 2. Renamed all references to "states_priorities" and "priority_per_state" to "hi_priorities".

--- xqtive_example/modules/xqtive_example_state_machine.py
+++ xqtive_example/modules/xqtive_example_state_machine.py
@@ -1,10 +1,10 @@
 from xqtive_states import States
 
 class XqtiveExampleStateMachine(States):
-    def __init__(self):
-        States.__init__(self)
+    def __init__(self, config):
+        States.__init__(self, config)
         new_state_priorities = {"PowerOff": 0}
-        self.state_priorities.update(new_state_priorities)
+        self.hi_priorities.update(new_state_priorities)
 
     def Message(self):
         print(f"Message {self.__dict__}")
