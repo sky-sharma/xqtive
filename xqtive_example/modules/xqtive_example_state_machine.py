2f145d949103c4148614b73ef2b4767bcbedf17e
skysharma <sky.sharma@gmail.com>
2019-11-23 05:28:02 -0600

    1. Added imports for time, random 2. Renamed all references to "Xqtive" to "XQtive" (cap 'Q'). 3. Renamed all private (sub) states to be named starting with '_'.

--- xqtive_example/modules/xqtive_example_state_machine.py
+++ xqtive_example/modules/xqtive_example_state_machine.py
@@ -1,9 +1,11 @@
-from xqtive import XqtiveStates
+import random
+import time
+from xqtive import XQtiveStates
 
-class XqtiveExampleStateMachine(XqtiveStates):
+class XQtiveExampleStateMachine(XQtiveStates):
     def __init__(self, config):
         self.cust_hi_priority_states = ["PWR_SPLY_OFF","E_STOP"]
-        XqtiveStates.__init__(self, config)
+        XQtiveStates.__init__(self, config)
 
     def MESSAGE(self):
         print(f"Message {self.__dict__}")
@@ -11,31 +13,37 @@ class XqtiveExampleStateMachine(XqtiveStates):
     def PWR_SPLY_OUTPUT(self, params):
         voltage_to_set = float(params[0])
         next_states_params = [
-        ["SetCurrentLimit", 1],
-        ["SetVoltage", voltage_to_set],
+        ["_SetCurrentLimit", 1],
+        ["_SetVoltage", voltage_to_set],
         ["WAIT", "20"],
-        ["CloseContactor"]]
+        ["_CloseContactor"]]
         self.feedback_msg = f"PWR_SPLY_OUTPUT; About to run sequence to set output voltage to {voltage_to_set}..."
         return next_states_params
 
     def PWR_SPLY_OFF(self):
-        next_states_params = ["SetVoltage", 0]
+        next_states_params = ["_SetVoltage", 0]
         self.feedback_msg = "PWR_SPLY_OFF; About to set output voltage to 0..."
         return next_states_params
 
     def E_STOP(self):
-        next_state_params = ["OpenContactor"]
+        next_state_params = ["_OpenContactor"]
         self.feedback_msg = "E_STOP; About to open contactor..."
         return next_state_params
 
-    def SetCurrentLimit(self, params):
+    def _SetCurrentLimit(self, params):
         self.feedback_msg = f"Setting current limit to {params[0]} Amps..."
 
-    def SetVoltage(self, params):
+    def _SetVoltage(self, params):
         self.feedback_msg = f"Setting voltage out to {params[0]} Volts..."
 
-    def CloseContactor(self):
+    def _CloseContactor(self):
         self.feedback_msg = "Closing contactor..."
 
-    def OpenContactor(self):
+    def _OpenContactor(self):
         self.feedback_msg = "Opening contactor..."
+
+    def _ReadPollVar(self, poll_var):
+        if poll_var == "NOW_SEC":
+            return (time.time() % 60)    # Seconds now
+        else:
+            return (random.random() * 100)
