e61e133843f42ae356fdef9268199a8beec5254d
skysharma <sky.sharma@gmail.com>
2019-11-22 21:42:09 -0600

    1. Changed references to hi_priorities to hi_priority_states. 2. Removed print statements. Replaced several with statements setting self.feedback_msg.

--- xqtive_example/modules/xqtive_example_state_machine.py
+++ xqtive_example/modules/xqtive_example_state_machine.py
@@ -2,7 +2,7 @@ from xqtive import XqtiveStates
 
 class XqtiveExampleStateMachine(XqtiveStates):
     def __init__(self, config):
-        self.cust_hi_priorities = {"PWR_SPLY_OUTPUT": "Normal", "PWR_SPLY_OFF": "High", "E_STOP": "Highest"}
+        self.cust_hi_priority_states = ["PWR_SPLY_OFF","E_STOP"]
         XqtiveStates.__init__(self, config)
 
     def MESSAGE(self):
@@ -10,35 +10,32 @@ class XqtiveExampleStateMachine(XqtiveStates):
 
     def PWR_SPLY_OUTPUT(self, params):
         voltage_to_set = float(params[0])
-        print(f"Setting output voltage to {voltage_to_set}")
         next_states_params = [
         ["SetCurrentLimit", 1],
         ["SetVoltage", voltage_to_set],
         ["WAIT", "20"],
         ["CloseContactor"]]
+        self.feedback_msg = f"PWR_SPLY_OUTPUT; About to run sequence to set output voltage to {voltage_to_set}..."
         return next_states_params
 
     def PWR_SPLY_OFF(self):
-        print("Turning off power!")
-        next_states_params = [
-        ["SetVoltage", 0],
-        ["WAIT", "5"],
-        ["OpenContactor"]]
+        next_states_params = ["SetVoltage", 0]
+        self.feedback_msg = "PWR_SPLY_OFF; About to set output voltage to 0..."
         return next_states_params
 
     def E_STOP(self):
-        print("Hitting E-Stop!")
         next_state_params = ["OpenContactor"]
+        self.feedback_msg = "E_STOP; About to open contactor..."
         return next_state_params
 
     def SetCurrentLimit(self, params):
-        print(f"Setting current limit to {params[0]} Amps.")
+        self.feedback_msg = f"Setting current limit to {params[0]} Amps..."
 
     def SetVoltage(self, params):
-        print(f"Setting voltage out to {params[0]} Volts.")
+        self.feedback_msg = f"Setting voltage out to {params[0]} Volts..."
 
     def CloseContactor(self):
-        print(f"Closing contactor.")
+        self.feedback_msg = "Closing contactor..."
 
     def OpenContactor(self):
-        print(f"Opening contactor.")
+        self.feedback_msg = "Opening contactor..."
