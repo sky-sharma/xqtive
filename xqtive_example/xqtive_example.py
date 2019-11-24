46dc4463d653d400957ead20952c7b365c5211f5
skysharma <sky.sharma@gmail.com>
2019-11-23 19:18:06 -0600

    1. Changed references to xqtive_example_state_machine to xqtive_example_state_machines. 2. Replaced XQtiveExampleStateMachine with ExampleController and ExampleResource. 3. Modified subsequent code to launch TWO state machines and exchange queues.

--- xqtive_example/xqtive_example.py
+++ xqtive_example/xqtive_example.py
@@ -22,12 +22,28 @@ import xqtive_helpers
 config = xqtive_helpers.read_config(config_filepath)
 config["sequences_dir"] = sequences_dir
 
-# Import State Machine Class and create a State Machine.
-from xqtive_example_state_machine import XQtiveExampleStateMachine
+# Import State Machine Class and create State Machines.
+from xqtive_example_state_machines import ExampleController, ExampleResource
 
-state_machine_launcher_returned = xqtive_helpers.launch_state_machine(XQtiveExampleStateMachine, config, certs_dir)
-state_machine_processes = state_machine_launcher_returned["processes"]
+# Launch Controller State Machine
+ctrl_launcher_returned = xqtive_helpers.launch_state_machine("controller", ExampleController, config, certs_dir)
+controller_processes = ctrl_launcher_returned["processes"]
+controller_sm = ctrl_launcher_returned["state_machine"]
+controller_queue = ctrl_launcher_returned["states_queue"]
 
+# Launch resource state machine
+resource_launcher_returned = xqtive_helpers.launch_state_machine("resource", ExampleResource, config, certs_dir)
+resource_processes = resource_launcher_returned["processes"]
+resource_sm = resource_launcher_returned["state_machine"]
+resource_queue = resource_launcher_returned["states_queue"]
+
+state_machine_processes = controller_processes + resource_processes
+
+# Exchange queues
+controller_sm._set_other_sm_queues([resource_queue])
+resource_sm._set_other_sm_queues([controller_queue])
+
+#state_machine_processes = controller_processes
 # Wait till all processes have exited before killing main process
 for state_machine_process in state_machine_processes:
     state_machine_process.join()
