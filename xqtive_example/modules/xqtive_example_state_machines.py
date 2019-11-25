ab10ab50b50776bfc416d3ac48c6e733b7282dc2
skysharma <sky.sharma@gmail.com>
2019-11-24 22:57:10 -0600

    In ExampleController class: 1. In __init__ set self.actual_v to None. 2. In "PWR_SPLY_OUTPUT" state / method changed ["WAIT", "20"] to ["WAIT", "10"]. 3. Modified "_SetVoltage" to use the resource state machine's queue to call its "_SetV" state. 4. Modified "_ReadPollVar" to return self.actual_v if poll_var is "VOLTS". 5. Added "_ActualV" which is called by resource state machine to set actual voltage.
    In ExampleResource class:
    1. Removed "EXEC_CMD" state / method.
    2. Added "_SetV" which sets actual V on the power supply and returns the actual value set to the controller state machine.

--- xqtive_example/modules/xqtive_example_state_machines.py
+++ xqtive_example/modules/xqtive_example_state_machines.py
@@ -5,6 +5,7 @@ from xqtive import XQtiveStateMachine
 class ExampleController(XQtiveStateMachine):
     def __init__(self, sm_name, config, all_sm_queues):
         self.cust_hi_priority_states = config["states"].get("cust_hi_priority_states")
+        self.actual_v = None
         XQtiveStateMachine.__init__(self, sm_name, config, all_sm_queues)
 
     def MESSAGE(self):
@@ -15,7 +16,7 @@ class ExampleController(XQtiveStateMachine):
         next_states_params = [
         ["_SetCurrentLimit", 1],
         ["_SetVoltage", voltage_to_set],
-        ["WAIT", "20"],
+        ["WAIT", "10"],
         ["_CloseContactor"]]
         self.feedback_msg = f"PWR_SPLY_OUTPUT; About to run sequence to set output voltage to {voltage_to_set}..."
         return next_states_params
@@ -35,6 +36,8 @@ class ExampleController(XQtiveStateMachine):
 
     def _SetVoltage(self, params):
         self.feedback_msg = f"Setting voltage out to {params[0]} Volts..."
+        rsrc_states_queue = self.all_sm_queues["resource"]["states_queue"]
+        rsrc_states_queue.put(["_SetV", str(params[0])], "from_other_sm_sub_state")
 
     def _CloseContactor(self):
         self.feedback_msg = "Closing contactor..."
@@ -43,15 +46,24 @@ class ExampleController(XQtiveStateMachine):
         self.feedback_msg = "Opening contactor..."
 
     def _ReadPollVar(self, poll_var):
-        if poll_var == "NOW_SEC":
-            return (time.time() % 60)    # Seconds now
+        if poll_var == "VOLTS":
+            return self.actual_v
         else:
-            return (random.random() * 100)
+            pass
+
+    def _ActualV(self, params):
+        self.actual_v = float(params[0])
+
+
 
 class ExampleResource(XQtiveStateMachine):
     def __init__(self, sm_name, config, all_sm_queues):
         self.cust_hi_priority_states = []
         XQtiveStateMachine.__init__(self, sm_name, config, all_sm_queues)
 
-    def EXEC_CMD(self, params):
-        print(f"Params {params}")
+    def _SetV(self, params):
+        time.sleep(20)
+        v_to_set = float(params[0])
+        v_to_return = v_to_set + 5
+        ctrl_states_queue = self.all_sm_queues["controller"]["states_queue"]
+        ctrl_states_queue.put(["_ActualV", str(v_to_return)], "from_other_sm_sub_state")
